e6d29a55e502b2e8358a9d2ee017b62cb2988acf
Daniel Kent <danielkent910@gmail.com>
2017-12-06 02:43:04 -0300

    order

--- app/Http/Controllers/IndexController.php
+++ app/Http/Controllers/IndexController.php
@@ -71,11 +71,11 @@ class IndexController extends Controller
 
         $locations = DB::table('entity')->where('entity.name', '=', $id)->leftJoin('locations', 'entity.locations', 'like', DB::raw("concat('%', locations.locations_id, '%')"))->select('locations.*')->get();
 
-        $sub_facets = DB::table('entity')->where('entity.name', '=', $id)->leftJoin('facets', 'entity.facets', 'like', DB::raw("concat('%', facets.facets_id, '%')"))->select(DB::raw('facets.id as facets_id'), DB::raw('facets.name as facets_name'), DB::raw('facets.type as facets_type'))->get();
+        $sub_facets = DB::table('entity')->where('entity.name', '=', $id)->leftJoin('facets', 'entity.facets', 'like', DB::raw("concat('%', facets.facets_id, '%')"))->select(DB::raw('facets.id as facets_id'), DB::raw('facets.name as facets_name'), DB::raw('facets.type as facets_type'))->orderBy('facets_type','desc')->get();
 
-        $sub_entities = DB::table('entity')->where('entity.name', '=', $id)->leftJoin('entity as second', 'entity.entities', 'like', DB::raw("concat('%', second.entity_id, '%')"))->select(DB::raw('second.name as entity_name'), DB::raw('second.type as entity_type'))->get();
+        $sub_entities = DB::table('entity')->where('entity.name', '=', $id)->leftJoin('entity as second', 'entity.entities', 'like', DB::raw("concat('%', second.entity_id, '%')"))->select(DB::raw('second.name as entity_name'), DB::raw('second.type as entity_type'))->orderBy('entity_type','desc')->get();
 
-        $sub_resources = DB::table('entity')->where('entity.name', '=', $id)->leftJoin('resources', 'entity.resources', 'like', DB::raw("concat('%', resources.resources_id, '%')"))->where('resources.published', '=', '1')->select(DB::raw('resources.name as resources_name'), DB::raw('resources.type as resources_type'))->get();
+        $sub_resources = DB::table('entity')->where('entity.name', '=', $id)->leftJoin('resources', 'entity.resources', 'like', DB::raw("concat('%', resources.resources_id, '%')"))->where('resources.published', '=', '1')->select(DB::raw('resources.name as resources_name'), DB::raw('resources.type as resources_type'))->orderBy('resources_type','desc')->get();
         return view('frontend.entity', compact('menus', 'entity','entity_types', 'facet_types', 'resource_types', 'facets', 'sub_facets', 'sub_entities','sub_resources', 'locations'));
 
     }
@@ -87,11 +87,11 @@ class IndexController extends Controller
         $facet_types=DB::table('facets')->distinct()-> get(['type']);
         $resource_types=DB::table('resources')->where('published', '=', '1')->distinct()-> get(['type']);
         $facet = DB::table('facets')->where('id', $id)->first();
-        $sub_entities = DB::table('facets')->where('facets.id', '=', $id)->leftJoin('entity', 'facets.entities', 'like', DB::raw("concat('%', entity.entity_id, '%')"))->select(DB::raw('entity.name as entity_name'), DB::raw('entity.type as entity_type'))->get();
+        $sub_entities = DB::table('facets')->where('facets.id', '=', $id)->leftJoin('entity', 'facets.entities', 'like', DB::raw("concat('%', entity.entity_id, '%')"))->select(DB::raw('entity.name as entity_name'), DB::raw('entity.type as entity_type'))->orderBy('enttity_type','desc')->get();
 
-        $sub_facets = DB::table('facets')->where('facets.id', '=', $id)->leftJoin('facets as second', 'facets.facets', 'like', DB::raw("concat('%', second.facets_id, '%')"))->select(DB::raw('second.id as facets_id'), DB::raw('second.name as facets_name'), DB::raw('second.type as facets_type'))->get();
+        $sub_facets = DB::table('facets')->where('facets.id', '=', $id)->leftJoin('facets as second', 'facets.facets', 'like', DB::raw("concat('%', second.facets_id, '%')"))->select(DB::raw('second.id as facets_id'), DB::raw('second.name as facets_name'), DB::raw('second.type as facets_type'))->orderBy('facets_type','desc')->get();
 
-        $sub_resources = DB::table('facets')->where('facets.id', '=', $id)->leftJoin('resources', 'facets.resources', 'like', DB::raw("concat('%', resources.resources_id, '%')"))->where('resources.published', '=', '1')->select(DB::raw('resources.name as resources_name'), DB::raw('resources.type as resources_type'))->get();
+        $sub_resources = DB::table('facets')->where('facets.id', '=', $id)->leftJoin('resources', 'facets.resources', 'like', DB::raw("concat('%', resources.resources_id, '%')"))->where('resources.published', '=', '1')->select(DB::raw('resources.name as resources_name'), DB::raw('resources.type as resources_type'))->orderBy('resources_type','desc')->get();
 
         return view('frontend.facet', compact('menus', 'facet','entity_types', 'facet_types', 'resource_types', 'sub_facets', 'sub_entities','sub_resources'));
     }
@@ -104,11 +104,11 @@ class IndexController extends Controller
         $resource_types=DB::table('resources')->where('published', '=', '1')->distinct()-> get(['type']);
         $resource = DB::table('resources')->where('name', $id)->first();
 
-        $sub_entities = DB::table('resources')->where('resources.name', '=', $id)->leftJoin('entity', 'resources.entities', 'like', DB::raw("concat('%', entity.entity_id, '%')"))->select(DB::raw('entity.name as entity_name'), DB::raw('entity.type as entity_type'))->get();
+        $sub_entities = DB::table('resources')->where('resources.name', '=', $id)->leftJoin('entity', 'resources.entities', 'like', DB::raw("concat('%', entity.entity_id, '%')"))->select(DB::raw('entity.name as entity_name'), DB::raw('entity.type as entity_type'))->orderBy('entity_type','desc')->get();
 
-        $sub_facets = DB::table('resources')->where('resources.name', '=', $id)->leftJoin('facets', 'resources.facets', 'like', DB::raw("concat('%', facets.facets_id, '%')"))->select(DB::raw('facets.id as facets_id'), DB::raw('facets.name as facets_name'), DB::raw('facets.type as facets_type'))->get();
+        $sub_facets = DB::table('resources')->where('resources.name', '=', $id)->leftJoin('facets', 'resources.facets', 'like', DB::raw("concat('%', facets.facets_id, '%')"))->select(DB::raw('facets.id as facets_id'), DB::raw('facets.name as facets_name'), DB::raw('facets.type as facets_type'))->orderBy('facets_type','desc')->get();
 
-        $sub_resources = DB::table('resources')->where('resources.name', '=', $id)->leftJoin('resources as second', 'resources.resources', 'like', DB::raw("concat('%', second.resources_id, '%')"))->where('resources.published', '=', '1')->select(DB::raw('second.name as resources_name'), DB::raw('second.type as resources_type'))->get();
+        $sub_resources = DB::table('resources')->where('resources.name', '=', $id)->leftJoin('resources as second', 'resources.resources', 'like', DB::raw("concat('%', second.resources_id, '%')"))->where('resources.published', '=', '1')->select(DB::raw('second.name as resources_name'), DB::raw('second.type as resources_type'))->orderBy('resources_type','desc')->get();
 
         return view('frontend.resource', compact('menus', 'resource','entity_types', 'facet_types', 'resource_types', 'sub_facets', 'sub_entities','sub_resources'));
     }
